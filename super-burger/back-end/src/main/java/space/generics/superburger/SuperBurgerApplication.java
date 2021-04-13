868e3f57f3c1682a9f9476a582f0fe0fc100e7fb
Yehor Bublyk <yehor.bk@gmail.com>
2021-04-13 14:16:53 +0300

    application.properties configured

--- super-burger/back-end/src/main/java/space/generics/superburger/SuperBurgerApplication.java
+++ super-burger/back-end/src/main/java/space/generics/superburger/SuperBurgerApplication.java
@@ -2,12 +2,13 @@ package space.generics.superburger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SuperBurgerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuperBurgerApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(SuperBurgerApplication.class, args);
+    }
 
 }
