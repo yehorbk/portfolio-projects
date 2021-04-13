003df071eaf81954944b6fc4523455c0331ed61e
Yehor Bublyk <yehor.bk@gmail.com>
2021-04-13 14:06:08 +0300

    project initialized

---
+++ super-burger/back-end/src/main/java/space/generics/superburger/SuperBurgerApplication.java
@@ -0,0 +1,13 @@
+package space.generics.superburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBurgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperBurgerApplication.class, args);
+	}
+
+}
