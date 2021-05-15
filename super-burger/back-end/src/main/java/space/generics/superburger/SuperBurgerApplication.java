e5ce398d852e34439ac87834e3ba6415b4e4f2ba
Yehor Bublyk <yehor.bk@gmail.com>
2021-05-15 17:10:57 +0300

    building war configured

--- super-burger/back-end/src/main/java/space/generics/superburger/SuperBurgerApplication.java
+++ super-burger/back-end/src/main/java/space/generics/superburger/SuperBurgerApplication.java
@@ -3,9 +3,10 @@ package space.generics.superburger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class SuperBurgerApplication {
+public class SuperBurgerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 	SpringApplication.run(SuperBurgerApplication.class, args);
